50a643978c888b675113a6b368938795da3fa273
Alexandre Abreu <alexandreabreu@comp.ufla.br>
2020-03-12 20:42:23 -0300

    Tut updated

--- blog/models.py
+++ blog/models.py
@@ -6,8 +6,8 @@ class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     text = models.TextField()
-    create_date = models.DateField(auto_now=True)
-    published_date = models.DateField(blank=True, null=True)
+    created_date = models.DateTimeField(auto_now=True)
+    published_date = models.DateTimeField(blank=True, null=True)
 
     def publish(self):
         self.published_date = timezone.now()
