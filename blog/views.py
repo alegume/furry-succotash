4d9e5a1a8df6bf2e78abb401a91056cfa535efa4
Alexandre Abreu <alexandreabreu@comp.ufla.br>
2020-05-20 18:27:38 -0300

    list, edit, remove users

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth import get_user_model
 
+from users.forms import CustomUserChangeForm
 from .models import Post, Comment, PostLike
 from .forms import PostForm, CommentForm
 
@@ -119,3 +121,32 @@ def comment_remove(request, pk):
     comment.delete()
 
     return redirect('post_detail', pk=comment.post.pk)
+
+@login_required
+def user_list(request):
+    UserModel = get_user_model()
+    users = UserModel.objects.all().order_by('birth_date')
+
+    return render(request, 'blog/user_list.html', {'users': users})
+
+@login_required
+def user_remove(request, pk):
+    UserModel = get_user_model()
+    user = get_object_or_404(UserModel, pk=pk)
+    user.delete()
+
+    return redirect('user_list')
+
+@login_required
+def user_edit(request, pk):
+    UserModel = get_user_model()
+    user = get_object_or_404(UserModel, pk=pk)
+    if request.method == "POST":
+        form = CustomUserChangeForm(request.POST, instance=user)
+        if form.is_valid():
+            form.save()
+            return redirect('user_list')
+    else:
+        form = CustomUserChangeForm(instance=user)
+
+    return render(request, 'blog/user_edit.html', {'form': form})
